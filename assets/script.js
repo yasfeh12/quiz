c5cbb8814a0ae943e1b74774014be0d69dee8860
Yaseen fehad <yaseenfehad@gmail.com>
2024-01-16 13:05:35 +0000

    added the questions for my quiz

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "What is the capital of England?",
+        answers: [
+            { text: "Leeds", correct: false },
+            { text: "Manchester", correct: false },
+            { text: "Edinburgh", correct: false },
+            { text: "London", correct: true },
+        ]
+    },
+    {
+        question: "What is the population of England in 2024?",
+        answers: [
+            { text: "71 million", correct: false },
+            { text: "57 million", correct: true },
+            { text: "52 million", correct: false },
+            { text: "64 million", correct: false },
+        ]
+    },
+    {
+        question: "Who is the monarch in England in 2024?",
+        answers: [
+            { text: "King James", correct: false },
+            { text: "King Andrew", correct: false },
+            { text: "King Charles", correct: true },
+            { text: "Queen Elizabeth", correct: false },
+        ]
+    },
+    {
+        question: "Which of these is not part of the UK?",
+        answers: [
+            { text: "England", correct: false },
+            { text: "Scotland", correct: false },
+            { text: "New Zealand", correct: true },
+            { text: "Wales", correct: false },
+        ]
+    },
+];
+
